df46457975dd74bb77b0de6721c53ab8b0a4121d
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-11-21 16:49:37 +0500

    Updates

---
+++ precache-manifest.fb8c0a4664b383984a4697dc5d7a4212.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c83aee8d7181c49bcb22fd0062ae371",
+    "url": "/deeplizard-clone/index.html"
+  },
+  {
+    "revision": "9d39ab6c881d2dfeebb5",
+    "url": "/deeplizard-clone/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "886cfa3af1b78ee01fa1",
+    "url": "/deeplizard-clone/static/css/main.7f8180fe.chunk.css"
+  },
+  {
+    "revision": "9d39ab6c881d2dfeebb5",
+    "url": "/deeplizard-clone/static/js/2.d6015e31.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/deeplizard-clone/static/js/2.d6015e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886cfa3af1b78ee01fa1",
+    "url": "/deeplizard-clone/static/js/main.9762f7d8.chunk.js"
+  },
+  {
+    "revision": "648e4f4f43f771b7488b",
+    "url": "/deeplizard-clone/static/js/runtime-main.f4acb264.js"
+  }
+]);
